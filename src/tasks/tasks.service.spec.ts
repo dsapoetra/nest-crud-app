54e687d4a97bbd6c563fd4bd0f6c3c1c8936ea8b
dsapoetra <user@Users-MacBook-Pro.local>
2024-11-11 09:27:54 +0700

    add more service task

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -25,4 +25,53 @@ describe('TasksService', () => {
       status: 'OPEN',
     });
   });
+
+  it('should retrieve all task', () => {
+    service.createTask('Test title', 'Test description');
+    const tasks = service.getAllTasks();
+    expect(tasks.length).toEqual(1);
+    expect(tasks[0].id).toEqual(1);
+  });
+
+  it('should retrieve task by id', () => {
+    service.createTask('Test title', 'Test description');
+    const task = service.getTaskById(1);
+    expect(task).toEqual({
+      id: 1,
+      title: 'Test title',
+      description: 'Test description',
+      status: 'OPEN',
+    });
+  });
+
+  it('should return undefined if retrieve task by not exists id', () => {
+    service.createTask('Test title', 'Test description');
+    const task = service.getTaskById(2);
+    expect(task).toBeUndefined();
+  });
+
+  it('should update task status', () => {
+    service.createTask('Test title', 'Test description');
+    const task = service.updateTaskStatus(1, 'DONE');
+    expect(task).toEqual({
+      id: 1,
+      title: 'Test title',
+      description: 'Test description',
+      status: 'DONE',
+    });
+  });
+
+  it('deleting a task', () => {
+    service.createTask('Test title', 'Test description');
+    service.deleteTask(1);
+    const tasks = service.getAllTasks();
+    expect(tasks.length).toEqual(0);
+  });
+
+  it('deleting an undefined task', () => {
+    service.createTask('Test title', 'Test description');
+    service.deleteTask(3);
+    const tasks = service.getAllTasks();
+    expect(tasks.length).toEqual(1);
+  });
 });
